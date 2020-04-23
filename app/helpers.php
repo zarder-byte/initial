3ba1bfc0cc7a8cfde02459fabe40682e0425eb2b
zarder-byte <2934108960@qq.com>
2020-04-23 09:24:57 +0800

    04-02-01前端基础结构

--- app/helpers.php
+++ app/helpers.php
@@ -1,3 +1,17 @@
 <?php
 
+<<<<<<< HEAD
 //公共函数库
+=======
+/**
+ * 自定义函数库
+ * 弹出信息提示框
+ * @param [type] $msg 要在网页上显示的提示信息
+ * @param string $type success / danger
+ * @return void
+ */
+
+ function alert($msg, $type='success'){
+     session()->flash($type,$msg);
+ }
+>>>>>>> 04-02-01
