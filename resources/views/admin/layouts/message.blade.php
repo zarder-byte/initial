3c348de573d041a673ac88598859cc0af71f23d7
zarder-byte <2934108960@qq.com>
2020-04-23 08:51:13 +0800

    前端基础样式

---
+++ resources/views/admin/layouts/message.blade.php
@@ -0,0 +1,18 @@
+<?php
+//session()->flash('danger','test');
+alert('test','success');
+?>
+@foreach(['success','danger'] as $message)
+@if(session()->has($message))
+<div class="message mt-2">
+    <div class="alert alert-{{$message}}">
+        {{session()->get($message)}}
+    </div>
+    <script>
+        setTimeout(function(){
+            $(".alert").alert('close');
+        },5000);
+    </script>
+</div>
+@endif
+@endforeach
