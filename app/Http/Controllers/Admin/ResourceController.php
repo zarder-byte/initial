715c7a3efe98baa9d1197801031a0450c727c11b
zarder-byte <2934108960@qq.com>
2020-05-16 22:48:43 +0800

    04-06-06 课程资源：数据组合搜索

--- app/Http/Controllers/Admin/ResourceController.php
+++ app/Http/Controllers/Admin/ResourceController.php
@@ -5,14 +5,34 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Resource;
 use Illuminate\Http\Request;
+use stdClass;
 
 class ResourceController extends Controller
 {
     //资源列表
     public function index(Request $request, Resource $resource){
+        $resource = $resource->with('adminuser');
+        //首先获取一个stdClass空对象
+        $stdclass = new stdClass;
+        $stdclass->keyword = $request->input('keyword','');
+        $stdclass->adminuser_id = $request->input('adminuser_id','');
+        $stdclass->type = $request->input('type',null);
+        if($stdclass->keyword){
+            $resource = $resource->where('title','like',"%{$stdclass->keyword}%");
+        }
+
+        if($stdclass->adminuser_id){
+            $resource = $resource->where('adminuser_id',$stdclass->adminuser_id);
+        }
+
+        if($stdclass->type){
+            $resource = $resource->whereIn('type',$stdclass->type);
+        }
+
         $resources = $resource->orderby('id','desc')->paginate(setting('page_resource'));
         $data = [
             'resources' => $resources,
+            'stdclass' =>$stdclass,
         ];
         return view('admin.resource.index',$data);
     }
