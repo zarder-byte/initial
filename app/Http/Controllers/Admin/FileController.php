bc701c0171334ddbfb002b0def6e319e0e37f412
zarder-byte <2934108960@qq.com>
2020-08-25 18:06:13 +0800

    04-08-03文件列表、04-08-04上传文件,合并上传

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -11,7 +11,10 @@ class FileController extends Controller
 {
     //文件管理列表页
     public function index(File $fileModel,AdminUser $adminUser){
-        $data = [];
+        $files = $fileModel->orderby('id','desc')->paginate(8);
+        $data = [
+            'files' => $files,
+        ];
         return view('admin.file.index',$data);
     }
 
@@ -23,6 +26,25 @@ class FileController extends Controller
 
     //处理添加的业务逻辑
     public function save(Request $request,File $fileModel){
+        //检查文件是否上传以及是否有效
+        if( $request->hasFile('filename') && $request->file('filename')->isValid()){
+            //获取上传的对象
+            $file = $request->file('filename');
+            //上传文件的类型是否允许
+            if(!in_array($file->extension(),config('project.upload.files'))){
+                alert('不被允许的上传类型','danger');
+                return redirect()->back();
+            }
+
+            //将图像保存到...
+            $filepath = $file->store('other','public');
+            $fileModel->saveFile('other_upload',$filepath,$file);
+            alert('上传成功');
+            return redirect()->route('admin.file');
+        }else{
+            alert('上传失败','danger');
+            return back();
+        }
 
     }
 
--- resources/views/admin/file/index.blade.php
+++ resources/views/admin/file/index.blade.php
@@ -11,12 +11,44 @@
 @section('content')
 @page_title(['title'=>'文件','conment'=>'管理你的文件'])
 
+@endpage_title()
+
 <div class="row">
     <div class="col-12">
-
+    <table class="table table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">作者</th>
+                    <th scope="col">类别</th>
+                    <th scope="col">类型</th>
+                    <th scope="col">大小</th>
+                    <th scope="col">文件名</th>
+                    <th scope="col">地址</th>
+                    <th scope="col">创建时间</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($files as $file)
+                <tr>
+                    <th scope="row">{{$file->id}}</th>
+                    <td>{{$file->adminuser->username}}</td>
+                    <td>{{$file->file_type_format}}</td>
+                    <td>{{$file->fileext}}</td>
+                    <td><a href='{{$file->file_link}}' target='_blank'>{{$file->file_size_format}}</a></td>
+                    <td>
+                        <input class='form-control form-control-sm' type='text' value='{{$file->client_filename}}' />
+                    </td>
+                    <td>
+                        <input class='form-control form-control-sm' type='text' value='{{$file->file_link}}' />
+                    </td>
+                    <td>{{$file->created_at}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{$files->links()}}
     </div>
 </div>
-
-@endpage_title()
 
 @endsection
